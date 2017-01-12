26578375952d455e8200a6e0979535b8204e5c4d
Valerie Dryden <outragedpinkracoon@gmail.com>
2017-01-12 16:09:46 +0000

    just doing the stuff

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'JS the good parts' },
-		{ title: 'Eloquent Ruby' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Harry Potter' }
+		{ title: 'JS the good parts', pages: 101 },
+		{ title: 'Eloquent Ruby', pages: 50 },
+		{ title: 'The Dark Tower', pages: 15 },
+		{ title: 'Harry Potter', pages: 100 }
 	]
 }
